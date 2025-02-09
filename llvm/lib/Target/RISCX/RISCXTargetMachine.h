703420a82022d826cb6b2407a5b7b4e1b464e307
circYuan <quanyueyuan@gmail.com>
2025-02-09 16:43:17 +0800

    test

---
+++ llvm/lib/Target/RISCX/RISCXTargetMachine.h
@@ -0,0 +1,12 @@
+#ifndef LLVM_LIB_TARGET_RISCX_RISCVXTARGETMACHINE_H
+#define LLVM_LIB_TARGET_RISCX_RISCVXTARGETMACHINE_H
+
+#include "llvm/CodeGen/CodeGenTargetMachineImpl.h"
+
+namespace llvm {
+
+class RISCXTargetMachine : public CodeGenTargetMachineImpl {};
+
+} // namespace llvm
+
+#endif
